a70d9d689f6abf2c1558b90f286fa02d294a3185
codeWithZain11 <143381607+codeWithZain11@users.noreply.github.com>
2024-05-08 11:49:15 +0530

    root

---
+++ src/main/java/com/ai/webconfig/WebConfig.java
@@ -0,0 +1,23 @@
+package com.ai.webconfig;
+
+
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.web.servlet.ViewResolver;
+	import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+	import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+	import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig implements WebMvcConfigurer {
+
+	    @Bean
+	    public ViewResolver viewResolver() {
+	        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+	        viewResolver.setPrefix("/static/templates/view/");
+	        viewResolver.setSuffix(".html");
+	        return viewResolver;
+	    }
+	}
+
